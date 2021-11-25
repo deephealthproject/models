ad4b79df424390b930f41d8e05085f8145966650
salvacarrion <salva.carrion@outlook.com>
2021-11-25 19:17:16 +0100

    Minor changes

--- development/keras2onnx.py
+++ development/keras2onnx.py
@@ -4,14 +4,14 @@ import tf2onnx
 import onnx
 
 
-def convert2onnx(model_path, savepath="model.onnx", input_size=(1, 256, 256, 3)):
-    # Load model
-    print("Loading model...")
+def convert2onnx(model_path, savepath="models.onnx", input_size=(1, 256, 256, 3)):
+    # Load models
+    print("Loading models...")
     model = tf.keras.models.load_model(filepath=model_path, compile=False)
     model.summary()
 
     # Convert to ONNX
-    print("Converting model to onnx...")
+    print("Converting models to onnx...")
     input_spec = (tf.TensorSpec(input_size, dtype=tf.float32),)
     onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=input_spec)
 
@@ -21,7 +21,7 @@ def convert2onnx(model_path, savepath="model.onnx", input_size=(1, 256, 256, 3))
 
 
 if __name__ == "__main__":
-    MODEL_PATH = os.path.join("model.h5")
-    SAVEPATH = "./model.onnx"
+    MODEL_PATH = os.path.join("models.h5")
+    SAVEPATH = "./models.onnx"
     INPUT_SIZE = (1, 256, 256, 3)
     convert2onnx(model_path=MODEL_PATH, savepath=SAVEPATH, input_size=INPUT_SIZE)
